9ec67e258eae3a012d1d2f7d842fbc01fbbb286f
jsalemfinancial <jsalemfinancial@gmail.com>
2022-10-03 21:01:38 -0400

    Overhaul #1
    - Redid Calculator to be in line with crafting changes. Fixed wonky math.
    - Entertained animated background idea.
    - Modified container graphics.
    - Modified layout and menu setup.

--- js/script.js
+++ js/script.js
@@ -1,40 +1,83 @@
 // main calculation script
 
 
+
 // Calculator class.
 let Calculate = new class {
     constructor() {
-        this.cSkill = null;
-        this.cDiff = null;
-        this.iProb = null;
-        this.qMax = 1;
-        this.iBonus = 0;
-        this.qGap = 0;
-        this.eCrafts = 0;
+        this.cSkill = null; // Crafting Skill.
+        this.cDiff = null; // Crafting Difficulty.
+        this.iProb = null; // Inspiration Probability.
+        // this.sIMax = 0; // Skill Level Cap by Inspiration.
+        // this.sLMax = 0; // Skill Level Cap by Level.
+        // this.sFinal = 0; //Final Decided Crafting Skill.
+        this.qMax = 1; // Max Quality.
+        this.qMin = 1; // Guaranteed Quality.
+        this.iBonus = 0; // Inspiration Bonus.
+        this.qGap = 0; // Quality Gap.
+        this.t4Ratio = 6.5;
+        this.t3Ratio = 2.8;
+        this.t2Ratio = 1.6;
+        this.eCrafts = 0; // Expected Crafts.
     }
 
     getParams() {
         this.iBonus = this.cDiff/6;
         this.qGap = this.cDiff/4;
-        this.qMax = Math.floor(5 - ((this.cDiff - (this.cSkill + this.iBonus))/this.qGap));
-        console.log(this.iBonus, this.qGap, this.qMax);
+        // this.sIMax = Math.floor(this.cSkill/this.iBonus)*this.iBonus;
+        // this.sLMax = Math.floor(this.cSkill/this.qGap)*this.qGap;
+        // this.sFinal = Math.max(this.sIMax, this.sLMax);
+        // this.qMax = Math.floor(5 - (this.cDiff - (this.cSkill + this.iBonus))/this.qGap);
+
+        if (this.cDiff - this.cSkill <= 0) {
+            this.qMin = 5;
+        } else if (this.cDiff/(this.cDiff - this.cSkill) > this.t4Ratio) {
+            this.qMin = 4;
+        } else if (this.cDiff/(this.cDiff - this.cSkill) > this.t3Ratio) {
+            this.qMin = 3;
+        } else if (this.cDiff/(this.cDiff - this.cSkill) > this.t2Ratio) {
+            this.qMin = 2;
+        } else {
+            this.qMin = 1;
+        };
+
+        if (this.cSkill + this.iBonus > this.cDiff) {
+            this.qMax = 5;
+        } else if (this.cSkill + this.iBonus > this.cDiff*(this.t3Ratio - 1)/this.t3Ratio) {
+            this.qMax = 4;
+        } else if (this.cSkill + this.iBonus > this.cDiff*(this.t2Ratio - 1)/this.t2Ratio) {
+            this.qMax = 3;
+        } else {
+            this.qMax = 2;
+        };
     };
 
-    geteCrafts() {
-        this.eCrafts = Math.ceil(Math.log(1 - ((this.cDiff - (this.cSkill + this.qGap*(5-this.qMax))))/this.iBonus)/Math.log(1 - this.iProb));
-        this.eCrafts = (this.eCrafts < 0) ? 0 : this.eCrafts;
+    geteCrafts() {  
+        // this.eCrafts = Math.ceil(Math.log(1 - ((this.cDiff - (this.sFinal + this.qGap*(5 - this.qMax))))/this.iBonus)/Math.log(1 - this.iProb));
+        // this.eCrafts = (this.eCrafts < 0) ? 0 : this.eCrafts;
+        // this.eCrafts = (this.eCrafts != this.eCrafts) ? 0 : this.eCrafts;
 
-        console.log(this.eCrafts);
+        if (this.qMax == this.qMin) {
+            this.eCrafts = 0;
+        } else if (this.qMax > this.qMin) {
+            this.eCrafts = Math.ceil(1/(this.iProb/100));
+        } else {
+            this.eCrafts = "Something Went Wrong!";
+        };
+
+        this.eCrafts = (this.eCrafts != this.eCrafts) ? "Does Not Apply" : this.eCrafts;
+        this.eCrafts = (this.eCrafts > 1000) ? "Does Not Apply" : this.eCrafts;
     };
 
     display() {
-        document.getElementsByName("iBonus")[0].getElementsByTagName("p")[0].innerHTML = this.iBonus;
+        document.getElementsByName("iBonus")[0].getElementsByTagName("p")[0].innerHTML = (this.iBonus).toFixed(2);
+        document.getElementsByName("qMin")[0].getElementsByTagName("p")[0].innerHTML = this.qMin;
         document.getElementsByName("qMax")[0].getElementsByTagName("p")[0].innerHTML = this.qMax;
         document.getElementsByName("eCrafts")[0].getElementsByTagName("p")[0].innerHTML = this.eCrafts;
     };
 }
 
-//Initial globals for stream.
+// Initial globals for stream.
 
 async function twitchMinimize() {
     clearTimeout(INIT_MINIMIZE);
@@ -54,15 +97,25 @@ const INIT_MINIMIZE = setTimeout(async () => {
 
 // Event listeners
 
+// document.querySelectorAll("input").forEach(input => {
+//     input.addEventListener('input', () => {
+//         Calculate[input.getAttribute("name")] = parseFloat(input.value);
+
+//         if (typeof(Calculate.cSkill) == "number" && typeof(Calculate.cDiff) == "number" && typeof(Calculate.iProb) == "number") {
+//             Calculate.getParams();
+//             Calculate.geteCrafts();
+//             Calculate.display();
+//         };
+//     });
+// });
+
 document.querySelectorAll("input").forEach(input => {
     input.addEventListener('input', () => {
         Calculate[input.getAttribute("name")] = parseFloat(input.value);
 
-        if (typeof(Calculate.cSkill) == "number" && typeof(Calculate.cDiff) == "number" && typeof(Calculate.iProb) == "number") {
-            Calculate.getParams();
-            Calculate.geteCrafts();
-            Calculate.display();
-        };
+        Calculate.getParams();
+        Calculate.geteCrafts();
+        Calculate.display();
     });
 });
 
